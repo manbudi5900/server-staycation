5474973fdfbdf1347d2089b5ae1fa386b2ceb7f6
manbudi5900 <manbudi590@gmail.com>
2023-08-12 15:51:40 +0800

    change env

--- config/config_db.go
+++ config/config_db.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"log"
-	"os"
 	"staycation/domain"
 
 	"github.com/joho/godotenv"
@@ -15,11 +14,17 @@ import (
 func InitDB() *gorm.DB  {
 	processENV()
 
-	dbUsername := os.Getenv("DB_USERNAME")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_DATABASE")
+	// dbUsername := os.Getenv("DB_USERNAME")
+	// dbPassword := os.Getenv("DB_PASSWORD")
+	// dbHost := os.Getenv("DB_HOST")
+	// dbPort := os.Getenv("DB_PORT")
+	// dbName := os.Getenv("DB_DATABASE")
+
+	var dbUsername = "edwzgbymhxftlx"
+	var dbPassword = "2ff13d0e42b4c64cebc75d88f9e6ada58b1b63b6470654ceb47d4fdfc1556565"
+	var dbHost = "ec2-52-215-68-14.eu-west-1.compute.amazonaws.com"
+	var dbPort = "5432"
+	var dbName ="ddu8acp2fpitg6"
 
 
 	dsn := "host="+dbHost+" user="+dbUsername+" password="+dbPassword+" dbname="+dbName+" port="+dbPort+" sslmode=disable TimeZone=Asia/Jakarta"
